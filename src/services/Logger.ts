9beaba94702c22e304ca213e13423388b1dc70ef
Maxim Zhemerenko <maxim.zhemerenko@outlook.com>
2018-04-13 01:23:34 +0300

    Mixpanel node lib used

--- src/services/Logger.ts
+++ src/services/Logger.ts
@@ -1,7 +1,38 @@
-import Analytics from 'analytics-node'
+const mixpanelKey = process.env.MIXPANEL_KEY;
 
-if (!process.env.ANALYTICS_KEY) {
-  throw new Error('Analytics key should be provided')
+if (!mixpanelKey) {
+  throw new Error('Mixpanel key should be provided')
 }
 
-export default new Analytics(process.env.ANALYTICS_KEY)
+const Mixpanel = require("mixpanel");
+
+const mixpanel = Mixpanel.init(mixpanelKey)
+
+export default class Logger {
+  public static track(eventName: string, userId: string, traits?: any) {
+    const data = Object.assign(
+      {distinct_id: userId},
+      traits
+    );
+
+    mixpanel.track(eventName, data)
+  }
+
+  public static trackError(userId: string, error: any) {
+    Logger.track("Error", userId, {error: error})
+  }
+
+  public static identify(userId: string, name: string) {
+    mixpanel.people.set(userId, {
+      $first_name: name
+    })
+  }
+
+  public static trackConversationStarted(userProfile: any) {
+    Logger.track("Conversation started", userProfile.id)
+  }
+
+  public static trackMessageReceived(text: string, userProfile: any) {
+    Logger.track("Message received", userProfile.id, {text: text})
+  }
+}
