07c74bfc02179338e9d86005913c451ff52ffecc
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-04-21 10:28:18 +0300

    Add automate deployment script

---
+++ deploy-now.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const cmd = require('node-cmd')
+const path = require('path')
+const program = require('commander')
+
+process.env.CLI = true;
+program
+  .option('--bot [bot]', 'Bot to deploy')
+  .option('--env [env]', 'Environment to deploy')
+  .parse(process.argv)
+
+const { env, bot } = program
+const name = `stand-bots-${env}-${bot}`
+
+const config = {
+  name,
+  public: true,
+  dotenv: `.env.${env}`,
+  env: {
+    // env variables can only be strings or numbers
+    "START_VIBER": Boolean(bot === 'viber').toString(),
+    "START_TELEGRAM": Boolean(bot === 'telegram').toString(),
+  },
+  files: [
+    ".env",
+    ".env.stage",
+    "src",
+    ".sequelizerc",
+    "tsconfig.json",
+    "migrations",
+    "ecosystem.config.js",
+    "typings.d.ts",
+    "now.json"
+  ]
+}
+
+cmd.get(`now rm ${name} --yes`, () => {
+  console.log('Previous deployment have been removed')
+})
+
+fs.writeFile('now.json', JSON.stringify(config), (error) => {
+  if (error) {
+    throw error
+  }
+
+  console.log('Successfully created config file')
+})
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
 // tslint:disable no-var-requires
 require("dotenv").config();
-
 import "./db";
+
+// make sure that env variables can only be strings
+if (process.env.START_TELEGRAM === "true") {
+  require("./bots/telegram");
+}
+
+if (process.env.START_VIBER === "false") {
+  require("./bots/viber");
+}
