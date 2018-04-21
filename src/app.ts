df224be8701f6ad77cd5a589f1cf31ba054f2700
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-04-21 11:14:23 +0300

    Fix typo on viber bot starting instantiation

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,6 @@ if (process.env.START_TELEGRAM === "true") {
   require("./bots/telegram");
 }
 
-if (process.env.START_VIBER === "false") {
+if (process.env.START_VIBER === "true") {
   require("./bots/viber");
 }
