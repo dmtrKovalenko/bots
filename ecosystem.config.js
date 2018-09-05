a278280b049ae29ecc3d731a5032f1fad835ca82
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-09-05 16:46:14 +0300

    Remove setting .env vars from ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,9 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve('.env.prod') })
-
-const customEnvKeys = ['NODE_ENV', 'NOW_URL', 'TEAMUP_TOKEN', 'TEAMUP_SUBCALENDAR_ID', 'DATABASE_URL', 'MIXPANEL_KEY', 'VIBER_BOT_TOKEN', 'TELEGRAM_BOT_TOKEN', 'REDIS_HOST', 'REDIS_PORT', 'REDIS_PASSWORD',]
-const prodEnv = Object.keys(process.env)
-  .filter(key => customEnvKeys.includes(key))
-  .reduce((obj, key) => {
-    obj[key] = process.env[key];
-    return obj;
-  }, {});
-
 module.exports = {
   apps: [
     {
       name: 'viber',
       script: 'build/app.js',
       env: {
-        ...prodEnv,
         START_VIBER: 'true',
       },
       watch: false
@@ -24,7 +12,6 @@ module.exports = {
       name: 'telegram',
       script: 'build/app.js',
       env: {
-        ...prodEnv,
         START_TELEGRAM: 'true',
       },
       watch: false
@@ -33,7 +20,6 @@ module.exports = {
       name: 'cron',
       script: 'build/app.js',
       env: {
-        ...prodEnv,
         START_CRON: 'true',
       },
       watch: false
@@ -42,7 +28,7 @@ module.exports = {
   deploy: {
     prod: {
       user: 'admin',
-      host: [process.env.NOW_URL],
+      host: ["ec2-18-185-112-145.eu-central-1.compute.amazonaws.com"],
       ref: 'origin/feature/monthly-meta',
       repo: 'https://github.com/dmtrKovalenko/stand-bots.git',
       path: '/home/admin/stand-bots',
